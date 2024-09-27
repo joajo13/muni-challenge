268db68608b3090795761a4f5dab7528aee3783e
juan <juanbejamingiupponi@gmail.com>
2024-09-27 20:13:36 -0300

    upd middleware

--- muni-challenge-node/middleware.js
+++ muni-challenge-node/middleware.js
@@ -2,23 +2,33 @@ import jwt from "jsonwebtoken";
 
 const jwtSecret = process.env.JWT_SECRET;
 
-export const verifyToken = (req, res, next) => {
-    const header = req.header("Authorization") || "";
-    const token = header.split(" ")[1];
+export const verifyToken = (role) => {
+    return (req, res, next) => {
+        const header = req.header("Authorization") || "";
+        const token = header.split(" ")[1];
 
-    if (!jwtSecret) {
-        throw new Error("SECRET_KEY environment variable is not set");
-    }
+        if (!jwtSecret) {
+            throw new Error("SECRET_KEY environment variable is not set");
+        }
 
-    if (!token) {
-        return res.status(401).json({ error: "No tienes permisos." });
-    }
+        if (!token) {
+            return res.status(401).json({ error: "No tienes permisos." });
+        }
 
-    try {
-        const payload = jwt.verify(token, jwtSecret);
-        req.user = payload.user;
-        next();
-    } catch (error) {
-        return res.status(403).json({ error: "No tienes permisos." });
+        try {
+            const payload = jwt.verify(token, jwtSecret);
+            req.user = payload.user;
+
+            console.log(payload);
+
+            if (role && payload.user.role !== role) {
+                return res.status(403).json({ error: "No tienes permisos." });
+            }
+
+            next();
+        } catch (error) {
+            console.log(error);
+            return res.status(403).json({ error: "No tienes permisos." });
+        }
     }
 }
--- muni-challenge-node/modules/tramites/routes.js
+++ muni-challenge-node/modules/tramites/routes.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { create, getAll, getById, updateComment, updateStatus } from "./controller.js";
 import { upload } from "../../config/multer.js";
 import { verifyToken } from "../../middleware.js";
+import ROLES from "../../constants/ROLES.js";
 
 const router = Router();
 
-router.get("", verifyToken, getAll);
-router.get("/:id", verifyToken, getById);
+router.get("", verifyToken(ROLES.ADMIN), getAll);
+router.get("/:id", verifyToken(ROLES.ADMIN), getById);
 router.post("/create", upload.single('archivo'), create);
-router.put("/update/:id/comment", verifyToken, updateComment);
-router.put("/update/:id/status", verifyToken, updateStatus);
+router.put("/update/:id/comment", verifyToken(ROLES.ADMIN), updateComment);
+router.put("/update/:id/status", verifyToken(ROLES.ADMIN), updateStatus);
 
 export default router;
